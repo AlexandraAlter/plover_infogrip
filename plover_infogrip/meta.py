526648c4b8f79427098bf188f46636843fd73c55
AlexandraAlter <alexandra.alter@protonmail.com>
2021-06-23 00:20:29 +0100

    Reorganized and fixed some keys

---
+++ plover_infogrip/meta.py
@@ -0,0 +1,14 @@
+
+def mode(ctx, arg):
+    """
+    """
+    action = ctx.copy_last_action()
+    return action
+
+
+def mod(ctx, arg):
+    """
+    """
+    action = ctx.copy_last_action()
+    return action
+
