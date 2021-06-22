da87b138082acd5dd7eb6ab7054817cfd97eebb7
AlexandraAlter <alexandra.alter@protonmail.com>
2021-06-23 00:20:14 +0100

    Corrected the key configuration

--- plover_infogrip/system.py
+++ plover_infogrip/system.py
@@ -5,14 +5,14 @@ from dataclasses import dataclass
 
 from plover.system import english_stenotype
 
-KEYS = ('M', 'A', 'C', '1', '2', '3', '4')
+KEYS = ('M-', 'A-', 'C-', '1-', '2-', '3-', '4-')
 
 IMPLICIT_HYPHEN_KEYS = ()
 SUFFIX_KEYS = ()
 NUMBER_KEY = ''
 NUMBERS = {}
 
-UNDO_STROKE_STENO = ''
+UNDO_STROKE_STENO = None
 
 ORTHOGRAPHY_RULES = english_stenotype.ORTHOGRAPHY_RULES
 ORTHOGRAPHY_RULES_ALIASES = english_stenotype.ORTHOGRAPHY_RULES_ALIASES
@@ -21,16 +21,16 @@ ORTHOGRAPHY_WORDLIST = None
 
 KEYMAPS = {
     'Gemini PR': {
-        'M': 'res1', 'A': 'O-', 'C': 'A-',
-        '1': 'H-', '2': 'P-', '3': 'T-', '4': 'S1-',
-        '1': 'R-', '2': 'W-', '3': 'K-', '4': 'S2-',
-
-        'M': 'res2', 'A': '-E', 'C': '-U',
-        '1': '-F', '2': '-P', '3': '-L', '4': '-T',
-        '1': '-R', '2': '-B', '3': '-G', '4': '-S',
+        'M-': ('res1', 'res2'),
+        'A-': ('O-', '-E'),
+        'C-': ('A-', '-U'),
+        '1-': ('H-', 'R-', '-F', '-R'),
+        '2-': ('P-', 'W-', '-P', '-B'),
+        '3-': ('T-', 'K-', '-L', '-G'),
+        '4-': ('S1-', 'S2-', '-T', '-S'),
 
         'no-op': (
-            '#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#A', '#B', '#C'
+            '#1', '#2', '#3', '#4', '#5', '#6', '#7', '#8', '#9', '#A', '#B', '#C',
             '-D', '-Z',
             '*1', '*2', '*3', '*4',
             'Fn', 'pwr',
